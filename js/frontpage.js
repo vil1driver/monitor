07b0d584b6e86fb65a4b27fbdff6ac1760de2264
vil1driver <vil1driver@gmail.com>
2015-10-31 21:23:26 +0100

    fix error alerte

--- js/frontpage.js
+++ js/frontpage.js
@@ -331,13 +331,13 @@ function RefreshData()
 	.success(function() {
 	//	console.log("connection success");
 		$('#popup_warning').fadeOut(fad_Duration);
-        $('#fade').fadeOut(fad_Duration);
+        $('#fade2').fadeOut(fad_Duration);
 	})
 	.error(function() {
 		console.log("error connection lost");
 		window.scrollTo(0,0);
 		$('#popup_warning').fadeIn(fad_Duration);
-		$('#fade').fadeIn(fad_Duration);
+		$('#fade2').fadeIn(fad_Duration);
 	});
 	
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
