107f3e187d7779708048c6324a903572d18b1f47
vil1driver <vil1driver@gmail.com>
2015-11-05 18:42:25 +0100

    Revert "auto detect access and use correct ip"
    This reverts commit dcae43a7454d571f2b2ced8c6864d856cabb4eac.

--- js/frontpage_settings_exemple.js
+++ js/frontpage_settings_exemple.js
@@ -84,8 +84,7 @@ var auto = 30000;							// défilement automatique, temps entre chaque changemen
 
 $(document).ready(function() {
         $.roomplan = 2;           // define roomplan in Domoticz and create items below. (0 = all devices, not limited to a roomplan)
-		$.domoticzLanUrl = "http://192.168.22.100:8080";	// url de connection à domoticz pour un accès local (à la maison)
-		$.domoticzWanUrl = "http://toto.com:8765";			// url de connection à domoticz pour un accès distant (depuis internet)
+		$.domoticzurl = "http://192.168.22.100:8080";
 	
         $.PageArray = [
 		
