7d63c483d8c8f1367334c103f2b699fb9827645f
vil1driver <vil1driver@gmail.com>
2016-12-18 16:15:56 +0100

    minor changes

--- js/mqtt_settings.js
+++ js/mqtt_settings.js
@@ -141,15 +141,20 @@ function onMessageArrived(message) {
 				if (message.nvalue == 0)	// status Off
 					mySwipe.next(); 		// page suivante
 					
-					// mySwipe.slide(1);	// vers page 2 (la première page est numéroté 0)
-					
 				break;
 				
-			// afficher la page où le status de la cafetière est visible
-			case "Cafetiere on": // nom du bouton de mise en route de la cafetière
+			
+			case "Cafetiere on": // nom du bouton pushOn de mise en route de la cafetière
 
-				if (message.nvalue == 1)
-					mySwipe.slide(1);	// passer à la page 2
+					mySwipe.slide(1);	// passe à la page 2
+					
+					// annonce la mise en route
+					var oAudio = document.getElementById('myaudio');
+					oAudio.src = "sounds/cofee.mp3";
+					oAudio.play();
+					
+					// affiche une popup text
+					lightbox_open('switch',25000,'café en préparation');	
 					
 				break;	
 				
@@ -173,18 +178,18 @@ function onMessageArrived(message) {
 
 				if (message.nvalue == 1){	// status On 
 					
-					//oAudio.src = "http://icecast.skyrock.net/s/natio_mp3_128k";
-					oAudio.src = "sounds/poubellejaune.mp3";
+					oAudio.src = "http://icecast.skyrock.net/s/natio_mp3_128k";
+					//oAudio.src = "sounds/poubellejaune.mp3";
 					oAudio.play();
 					
 				}	
 				
 				if (message.nvalue == 0){	// status Off
 				
-					//oAudio.pause();	// stop
-					//oAudio.src = "";
-					oAudio.src = "sounds/poubellegrise.mp3";
-					oAudio.play();
+					oAudio.pause();	// stop
+					oAudio.src = "";
+					//oAudio.src = "sounds/poubellegrise.mp3";
+					//oAudio.play();
 					
 				}	
 				
@@ -195,9 +200,9 @@ function onMessageArrived(message) {
 			case "d3": // nom du bouton de la télécommande
 				
 				if (message.nvalue == 1){		// status On 
-					var source = "http://www.saintveranmeteo.eu/villagesaintveranwebcam.jpg";
+					var source = "http://www.saintveranmeteo.eu/villagesaintveranwebcam.jpg";	// source du flux de la caméra
 					$('#popup_camera').html('<img src='+source+' >');	// charge le flux dans la popup caméra
-					lightbox_open('camera', 25400);	// afficher la popup
+					lightbox_open('camera', 25000);	// afficher la popup 25 secondes
 				}	
 				if (message.nvalue == 0)		// status Off
 					lightbox_close('camera');	// fermer la popup
@@ -205,41 +210,40 @@ function onMessageArrived(message) {
 				break;	
 				
 			
-			// annoncer le saint du jour
-			case "saint": // nom du widget text
+			// annonce vocale suivant le contenu d'un dummy text
+			case "tts": // nom du widget text
 			
-				// en cours de dev..
-				
-				//var text = message.svalue1;
-				//var oAudio = document.getElementById('myaudio');
-				//oAudio.src = "http://www.voxygen.fr/sites/all/modules/voxygen_voices/assets/proxy/index.php?method=redirect&text="+text+"&voice=Marion";
-				//oAudio.play();
+								
+				var text = message.svalue1;
+				var oAudio = document.getElementById('myaudio');
+				oAudio.src = "http://www.voxygen.fr/sites/all/modules/voxygen_voices/assets/proxy/index.php?method=redirect&text="+text+"&voice=Fabienne";
+				oAudio.play();
 				
 				break;
 				
 			// afficher une page précise suivant le choix d'un switch sélecteur
 			case "page": // nom du switch sélecteur
 				
-				if (message.svalue1 == 10)   // Page 1 
+				if (message.svalue1 == 10)   // sélecteur à 10 %
 				   mySwipe.slide(0);   // vers page 1 (la première page est numéroté 0)
-				if (message.svalue1 == 20)   // Page 2
-				   mySwipe.slide(1);   // vers page 2 (la première page est numéroté 0)
-				if (message.svalue1 == 30)   // Page 3 
-				   mySwipe.slide(2);   // vers page 3 (la première page est numéroté 0)
-				if (message.svalue1 == 40)   // Page 4
-				   mySwipe.slide(3);   // vers page 4 (la première page est numéroté 0)
-				if (message.svalue1 == 50)   // Page 5 
-				   mySwipe.slide(4);   // vers page 5 (la première page est numéroté 0)
-				if (message.svalue1 == 60)   // Page 6
-				   mySwipe.slide(5);   // vers page 6 (la première page est numéroté 0)
-				if (message.svalue1 == 70)   // Page 7 
-				   mySwipe.slide(6);   // vers page 7 (la première page est numéroté 0)
-				if (message.svalue1 == 80)   // Page 8
-				   mySwipe.slide(7);   // vers page 8 (la première page est numéroté 0)
-				if (message.svalue1 == 90)   // Page 9 
-				   mySwipe.slide(8);   // vers page 9 (la première page est numéroté 0)
-				if (message.svalue1 == 100)   // Page 10
-				   mySwipe.slide(9);   // vers page 10 (la première page est numéroté 0)
+				if (message.svalue1 == 20)
+				   mySwipe.slide(1);   // vers page 2
+				if (message.svalue1 == 30)
+				   mySwipe.slide(2);   // vers page 3
+				if (message.svalue1 == 40)
+				   mySwipe.slide(3);   // vers page 4
+				if (message.svalue1 == 50)
+				   mySwipe.slide(4);   // vers page 5
+				if (message.svalue1 == 60)
+				   mySwipe.slide(5);   // vers page 6
+				if (message.svalue1 == 70)
+				   mySwipe.slide(6);   // vers page 7
+				if (message.svalue1 == 80)
+				   mySwipe.slide(7);   // vers page 8
+				if (message.svalue1 == 90)
+				   mySwipe.slide(8);   // vers page 9
+				if (message.svalue1 == 100)
+				   mySwipe.slide(9);   // vers page 10
 				   
 				break;	
 	} 
