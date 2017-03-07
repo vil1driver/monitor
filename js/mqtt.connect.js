cbec2cb3245ad45f6e0b0811d53233ce4a170876
Olivier <vil1driver@gmail.com>
2017-03-07 12:24:13 +0100

    auto reconnect to mqqt broker

--- js/mqtt.connect.js
+++ js/mqtt.connect.js
@@ -1,15 +1,17 @@
 ﻿// Create a client instance
 // mosquitto doit tourner sur la même machine que monitor et domoticz
 // dans le cas contraire, indiquer l'ip de la machine a la palce de location.hostname
-client = new Paho.MQTT.Client(location.hostname, 9001, '', 'monitor_'+Date.now());
+client = new Paho.MQTT.Client(location.hostname, 9001, '', 'monitor_'+Date.now().toString().match(/\d{5}$/));
 
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
-// connect the client
-client.connect({onSuccess:onConnect});
 
+// connect the client
+function Connect() {
+	client.connect({onSuccess:onConnect,onFailure:onSocketError});
+}
 
 // called when the client connects
 function onConnect() {
@@ -24,7 +26,22 @@ function onConnect() {
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
+    console.warn("Error MQTT : Connection Lost "+responseObject.errorMessage+" Reconnect will be attempted in 30 seconds.");
+	setTimeout(function() {
+	  Connect();
+	}, 30000);
   }
 }
+
+// called when Error in connection establishment
+function onSocketError(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.warn("Error MQTT : Connection Failed "+responseObject.errorMessage+" Reconnect will be attempted in 60 seconds.");
+	setTimeout(function() {
+	  Connect();
+	}, 60000);
+  }
+}
+
+Connect();
 
